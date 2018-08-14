fcb5e98fae78a8a140818aa7b039f91056975590
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-08-14 15:13:14 +0200

    switching pc's

--- src/main.cpp
+++ src/main.cpp
@@ -33,8 +33,7 @@ const char *mqttPswd = "31IMHCdWxVVt";
 TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
 PubSubClient mqtt(client);
-///////////////////////////////////// MQTT specific ////////////////////////////
-char mqttCl[12] = "";
+char mqttClient[12] = "";
 ///////////////////////////////////// RADIO ////////////////////////////////////
 #include <RF24.h>
 #include <RF24Network.h>
@@ -42,7 +41,7 @@ char mqttCl[12] = "";
 RF24 radio(radioPin1, radioPin2); // start RF24 communication layer
 RF24Network network(radio);       // start RF24 network layer
 const uint16_t thisNode = 00;     // Coordinator address
-////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// Wire /////////////////////////////////////
 #include <Wire.h>
 ///////////////////////////////////// RTC //////////////////////////////////////
 #include "uRTCLib.h"
@@ -51,13 +50,10 @@ const uint8_t interval = 2;
 uint8_t nextSend;
 uint8_t coordId[4] = {0xAA,0xFF,0x00,0xFF};
 //////////////////////////////// own libraries /////////////////////////////////
-//#include "RandomNodeId.h" // NodeId
 #include "MesureVoltageInternal.h" // Internal Voltage measurement
 float iREF = 1.1;
 MesureVoltageInternal battery(iREF);
-
 ///////////////////////////////////// STRUCTS & STRUCT ARAYS ///////////////////
-// Structure of our payload coming from router and end devices
 struct Payload_t {
   uint8_t id[4];
   int16_t temp[6];
@@ -67,7 +63,7 @@ struct Payload_t {
   uint8_t alarm;
 };
 
-struct plBuffer_t {
+struct payloadBuffer_t {
   uint8_t id[4];
   int16_t temp[6];
   uint16_t bat;
@@ -89,7 +85,7 @@ char s2[24];
 //char buf2[120] = "";
 // Globalstruct array to collect data before Sending
 #define BUFFERSIZE 6
-plBuffer_t plBuffer[BUFFERSIZE];
+payloadBuffer_t payloadBuffer[BUFFERSIZE];
 ////////////////////////////////////////////////////////////////////////////////
 #define numberOfSensors 6
 ////////////////////////// FUNCTION DECLARATIONS ///////////////////////////////
@@ -109,7 +105,7 @@ void sendMqttData(LocalData_t *local);
 void initRFRadio(uint8_t channel, uint16_t nodeAddress);
 void checkForNetworkData();
 // misc Functions
-void clearplBuffer();
+void clearpayloadBuffer();
 
 /////////////// SETUP //////////////////////////////////////////////////////////
 void setup() {
@@ -124,8 +120,8 @@ void setup() {
   for (byte b : coordId)
     Serial.print(b, HEX);
   Serial.println();
-  sprintf(mqttCl, "CO%02X%02X%02X%02X",coordId[0],coordId[1],coordId[2],coordId[3]);
-  Serial.println(mqttCl);
+  sprintf(mqttClient, "CO%02X%02X%02X%02X",coordId[0],coordId[1],coordId[2],coordId[3]);
+  Serial.println(mqttClient);
 
   battery.setRefInternal();
   //myHumidity.begin();
@@ -136,7 +132,7 @@ void setup() {
 
   registerNodeMqtt();
 
-  clearplBuffer();
+  clearpayloadBuffer();
   rtc.refresh();
   nextSend = rtc.minute();
   //Serial.println(F("init complete"));
@@ -155,7 +151,7 @@ void loop() {
     getLocalData(&localData);
     getScaleData(&localData);
     sendMqttData(&localData);
-    clearplBuffer();
+    clearpayloadBuffer();
   }
   Serial.println(F("sleep"));
   delay(500); // give serial time to complete before node goes to sleep
@@ -182,17 +178,39 @@ void fillBufferArray(Payload_t *payloadAddress) {
   uint8_t bufferLocation = 0;
   // get next free buffer location
   for (int i = 0; i < BUFFERSIZE; i++) {
-    if (plBuffer[bufferLocation].temp[0] != 0)
+    if (payloadBuffer[bufferLocation].temp[0] != 0)
       bufferLocation++;
   }
   Serial.print(F(" Array position "));
   Serial.println(bufferLocation); // print the buffer location that is used
   for (int i = 0; i < 4; i++)
-    plBuffer[bufferLocation].id[i] = payloadAddress->id[i];
+    payloadBuffer[bufferLocation].id[i] = payloadAddress->id[i];
   for (int i = 0; i < numberOfSensors; i++)
-    plBuffer[bufferLocation].temp[i] = payloadAddress->temp[i];
-  plBuffer[bufferLocation].humidity = payloadAddress->humidity;
-  plBuffer[bufferLocation].bat = payloadAddress->bat;
+    payloadBuffer[bufferLocation].temp[i] = payloadAddress->temp[i];
+  payloadBuffer[bufferLocation].humidity = payloadAddress->humidity;
+  payloadBuffer[bufferLocation].bat = payloadAddress->bat;
+}
+
+void saveDataToEep(Payload_t *payloadAddress){
+    uint8_t eepRomLocation = 1;
+  // get next free buffer location
+  for (int i = 1; i < BUFFERSIZE; i++) {
+    char ad = 0;
+    rtc.eeprom_read(i*25,ad,1);
+    if (ad != 0)
+      eepRomLocation++; //increase buffer location
+    else
+      break; // use current location
+  }
+  
+  Serial.print(F(" Eeprom position "));
+  Serial.println(eepRomLocation*25); // print the buffer location that is used
+  for (int i = 0; i < 4; i++)
+    rtc.eeprom_write((eepRomLocation*25)+i,payloadAddress->id[i]);
+  for (int i = 0; i < numberOfSensors; i++)
+    rtc.eeprom_write(30+(eepRomLocation*10)+(i*2),payloadAddress->temp[i]);
+  rtc.eeprom_write(42+(eepRomLocation*10), payloadAddress->humidity);
+  rtc.eeprom_write(44+(eepRomLocation*10), payloadAddress->bat);
 }
 
 void getLocalData(LocalData_t *local) {
@@ -205,7 +223,6 @@ void getLocalData(LocalData_t *local) {
 }
 
 void getScaleData(LocalData_t *local) {
-
   Wire.requestFrom(1, 24);
   delay(100);
   int i = 0;
@@ -220,23 +237,13 @@ void getScaleData(LocalData_t *local) {
     s2[i] = Wire.read();
     i++;
   }
+  s1[23] = 0;
+  s2[23] = 0;
 
-s1[23] = 0;
-s2[23] = 0;
-
-for (size_t i = 0; i < 23; i++) {
+  for (size_t i = 0; i < 23; i++) {
   Serial.print(s1[i]);
-}
-Serial.println();
-  /*for (int a = 0; a < 2; a++) {
-    Wire.requestFrom(1, 24);
-    delay(1000);
-    uint8_t i = 0;
-    while (Wire.available()) {
-      char c = Wire.read();
-      local->scales[a][i] = c;
-    }
-  }*/
+  }
+  Serial.println();
   Serial.println(s1);
   Serial.println(s2);
 }
@@ -245,8 +252,8 @@ Serial.println();
 void registerNodeMqtt() {
   gprsResetModem();
   gprsConnectNetwork();
-  if (mqtt.connect(mqttCl, mqttUser, mqttPswd)) {
-    mqtt.publish("c/r", mqttCl);
+  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+    mqtt.publish("c/r", mqttClient);
   }
   gprsEnd();
 }
@@ -255,15 +262,16 @@ void sendMqttData(LocalData_t *local) {
   gprsResetModem();
   gprsConnectNetwork();
   for (int b = 0; b < BUFFERSIZE; b++) {
-    if (plBuffer[b].temp[0] != 0) {
+    if (payloadBuffer[b].temp[0] != 0) {
       Serial.print(F("Send buffer "));
       Serial.println(b);
-      if (mqtt.connect(mqttCl, mqttUser, mqttPswd)) {
-        char buf[120] = "";
-        sprintf(buf, "%02X%02X%02X%02X,%d,%d,%d,%d,%d,%d,%u,%u,%u,%u,%d", plBuffer[b].id[0], plBuffer[b].id[1], plBuffer[b].id[2], plBuffer[b].id[3], plBuffer[b].temp[0], plBuffer[b].temp[1], plBuffer[b].temp[2], plBuffer[b].temp[3], plBuffer[b].temp[4], plBuffer[b].temp[5],plBuffer[b].humidity,plBuffer[b].bat,local->baseTemp,local->baseHum,local->baseLux);
+      if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+        char buf[120] = ""; // als ik deze als globale variabele declareer werkt het ook niet
+        sprintf(buf, "%02X%02X%02X%02X,%d,%d,%d,%d,%d,%d,%u,%u,%u,%u,%d", payloadBuffer[b].id[0], payloadBuffer[b].id[1], payloadBuffer[b].id[2], payloadBuffer[b].id[3], payloadBuffer[b].temp[0], payloadBuffer[b].temp[1], payloadBuffer[b].temp[2], payloadBuffer[b].temp[3], payloadBuffer[b].temp[4], payloadBuffer[b].temp[5],payloadBuffer[b].humidity,payloadBuffer[b].bat,local->baseTemp,local->baseHum,local->baseLux);
         mqtt.publish("h/d", buf);
+        // als ik deze lijnen uncomment werkt het ook niet
         //char buf2[80] = "";
-        //sprintf(buf2, "%s,%s,%s", mqttCl,s1,s2);
+        //sprintf(buf2, "%s,%s,%s", mqttClient,s1,s2);
         //mqtt.publish("c/s", buf2);
       }
       /*
@@ -321,8 +329,8 @@ void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
 }
 
 //////////// Misc Functions ////////////////////////////////////////////////////
-void clearplBuffer() {
+void clearpayloadBuffer() {
   for (int i = 0; i < BUFFERSIZE; i++) {
-    plBuffer[i].temp[0] = 0;
+    payloadBuffer[i].temp[0] = 0;
   }
 }
