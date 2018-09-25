ab1704ecca6279931fe9a8ef089b4cebc178b3e7
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-09-25 05:53:04 +0200

    removed a lot of data to save memory as much as posible - not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
 #include <Arduino.h>
 
+#define DEBUG
+#define DEBUG2
+#define DEBUGGPRS
+#ifdef DEBUG
+ #define debugPrint(x)  Serial.print (x)
+ #define debugPrintLn(x)  Serial.println (x)
+#else
+ #define debugPrint(x)
+ #define debugPrintLn(x)
+#endif
+
+#ifdef DEBUG2
+ #define debug2Print(x)  Serial.print (x)
+ #define debug2PrintLn(x)  Serial.println (x)
+ #define debug2PrintHex(x)  Serial.println (x, HEX)
+#else
+ #define debug2Print(x)
+ #define debug2PrintLn(x)
+ #define debug2PrintHex(x)
+#endif
+
+#ifdef DEBUGGPRS
+ #define debugGprsPrint(x)  Serial.print (x)
+ #define debugGprsPrintLn(x)  Serial.println (x)
+ #define debug2GprsPrintHex(x)  Serial.println (x, HEX)
+#else
+ #define debugGprsPrint(x)
+ #define debugGprsPrintLn(x)
+ #define debugGprsPrintHex(x)
+#endif
+
 ////////////////////////////////////////////////////////////////////////////////
 #define radioPin1 8 // RF24
 #define radioPin2 9 // RF24
@@ -63,49 +94,110 @@ struct Payload_t {
   uint8_t alarm;
 };
 
-struct payloadBuffer_t {
+struct plBuffer_t {
   uint8_t id[4];
   int16_t temp[6];
   uint16_t bat;
   uint16_t humidity;
 };
 
-struct LocalData_t {
-  uint8_t baseId[4];
-  int16_t baseTemp;
-  uint16_t baseHum;
-  uint16_t baseLux;
-  uint16_t baseBat;
-  char scales[2][24];
-};
-
-char s1[24];
-char s2[24];
-//char buf[120];
-//char buf2[120] = "";
-// Globalstruct array to collect data before Sending
-#define BUFFERSIZE 6
-payloadBuffer_t payloadBuffer[BUFFERSIZE];
+char buf[60];
+char buf2[50];
+char bufLocal[16] = "3412,6732,33333";
+plBuffer_t plBuffer[7];
 ////////////////////////////////////////////////////////////////////////////////
 #define numberOfSensors 6
-////////////////////////// FUNCTION DECLARATIONS ///////////////////////////////
-// getting data
-void fillBufferArray(Payload_t *payloadAddress);
-void getLocalData(LocalData_t *local);
-void getScaleData(LocalData_t *local);
-// gprs network functions
-void gprsResetModem();
-void gprsConnectNetwork();
-void gprsEnd();
-// sending data
-void registerNodeMqtt();
-// MQTT specific
-void sendMqttData(LocalData_t *local);
-// radio functions
-void initRFRadio(uint8_t channel, uint16_t nodeAddress);
-void checkForNetworkData();
-// misc Functions
-void clearpayloadBuffer();
+
+
+void clearplBuffer() {
+  for (int i = 0; i < 7; i++) {
+    plBuffer[i].temp[0] = 0;
+  }
+}
+
+//////////// init gprs, connect and disconnect from network ////////////////////
+void gprsResetModem() {
+  modem.restart();
+  String modemInfo = modem.getModemInfo();
+  debugGprsPrint(F(" Modem: "));
+  debugGprsPrintLn(modemInfo);
+}
+
+void gprsConnectNetwork() {
+  debugPrint(F(" Waiting for network..."));
+  if (!modem.waitForNetwork()) {
+    debugPrintLn(F(" fail"));
+    delay(10000);
+    return;
+  }
+  debugGprsPrintLn(F(" OK"));
+  debugGprsPrint(F(" Connecting to "));
+  debugGprsPrint(apn);
+  if (!modem.gprsConnect(apn, user, pass)) {
+    debugGprsPrintLn(F(" fail"));
+    delay(20000);
+    return;
+  }
+  debugGprsPrintLn(F(" OK"));
+}
+
+void gprsEnd() {
+  modem.gprsDisconnect();
+}
+
+//////////// init nRf radio ////////////////////////////////////////////////////
+void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
+  debugPrint(F("Channel: "));
+  debugPrint(channel);
+  debugPrint(F(", Node: "));
+  debugPrintLn(nodeAddress);
+
+  SPI.begin();
+  radio.begin();
+  // radio.setPALevel(HIGH);
+  network.begin(channel, nodeAddress);
+  network.setup_watchdog(9);
+}
+
+//////////// Mqtt functions ////////////////////////////////////////////////////
+
+void registerNodeMqtt() {
+  gprsResetModem();
+  gprsConnectNetwork();
+  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+    debug2PrintLn("Mqtt connected");
+    mqtt.publish("c/r", mqttClient);
+  }
+  gprsEnd();
+}
+
+void sendScaleMqtt(){
+  gprsResetModem();
+  gprsConnectNetwork();
+  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
+    debug2PrintLn("Mqtt connected");
+    mqtt.publish("c/s", buf);
+  }
+  mqtt.disconnect();
+  gprsEnd();
+}
+
+void sendDataMqtt() {
+  gprsResetModem();
+  gprsConnectNetwork();
+  for (int b = 0; b < 1; b++) {
+    if (plBuffer[b].temp[0] != 0) {
+      debugPrint(F("Send buffer "));
+      debugPrintLn(b);
+      sprintf(buf2, "%02X%02X%02X%02X,%d,%d,%d,%d,%d,%d,%u,%u,%s", plBuffer[b].id[0], plBuffer[b].id[1], plBuffer[b].id[2], plBuffer[b].id[3], plBuffer[b].temp[0], plBuffer[b].temp[1], plBuffer[b].temp[2], plBuffer[b].temp[3], plBuffer[b].temp[4], plBuffer[b].temp[5],plBuffer[b].humidity,plBuffer[b].bat,bufLocal);
+      //Serial.println(buf2);
+      mqtt.connect(mqttClient, mqttUser, mqttPswd);
+      mqtt.publish("h/d", buf2);
+    }
+  }
+  mqtt.disconnect();
+  gprsEnd();
+}
 
 /////////////// SETUP //////////////////////////////////////////////////////////
 void setup() {
@@ -114,28 +206,104 @@ void setup() {
   SerialAT.begin(9600);
   delay(1000);
 
-  Serial.print(F("BeeNode v0.1"));
+  debugPrint(F("BeeNode v0.1"));
   //beeNodeId.getId(coordId);
-  Serial.print(F(", Id: CO"));
+  debugPrint(F(", Id: CO"));
   for (byte b : coordId)
     Serial.print(b, HEX);
   Serial.println();
   sprintf(mqttClient, "CO%02X%02X%02X%02X",coordId[0],coordId[1],coordId[2],coordId[3]);
-  Serial.println(mqttClient);
+  debugPrintLn(mqttClient);
 
   battery.setRefInternal();
   //myHumidity.begin();
   //lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
   initRFRadio(90, thisNode); // start nRF24l radio
-
   mqtt.setServer(broker, mqttPort);
-
   registerNodeMqtt();
 
-  clearpayloadBuffer();
   rtc.refresh();
   nextSend = rtc.minute();
-  //Serial.println(F("init complete"));
+  clearplBuffer();
+}
+
+void getScaleData() {
+  char sbuf[2][24];
+  int i = 0;
+  for (size_t a = 0; a < 2; a++) {
+    i = 0;
+    Wire.requestFrom(1, 24);
+    //delay(100);
+    while (Wire.available() && i < 23) {
+      sbuf[a][i] = Wire.read();
+      i++;
+    }
+    sbuf[a][i] = '\0';
+  }
+  sprintf(buf, "%s,%s,%s", mqttClient, sbuf[0], sbuf[1]);
+  debug2PrintLn(buf);
+}
+
+void fillBufferArray(Payload_t *payloadAddress) {
+  uint8_t bufferLocation = 0;
+  for (int i = 0; i < 7; i++) {
+    if (plBuffer[bufferLocation].temp[0] != 0)
+      bufferLocation++;
+  }
+  debugPrint(F(" Array position "));
+  debugPrintLn(bufferLocation); // print the buffer location that is used
+  for (int i = 0; i < 4; i++)
+    plBuffer[bufferLocation].id[i] = payloadAddress->id[i];
+  for (int i = 0; i < numberOfSensors; i++)
+    plBuffer[bufferLocation].temp[i] = payloadAddress->temp[i];
+  plBuffer[bufferLocation].humidity = payloadAddress->humidity;
+  plBuffer[bufferLocation].bat = payloadAddress->bat;
+}
+
+void fillBufferArray(Payload_t *payloadAddress) {
+  uint8_t bufferLocation = 0;
+  for (int i = 0; i < 7; i++) {
+    if (plBuffer[bufferLocation].temp[0] != 0)
+      bufferLocation++;
+  }
+  debugPrint(F(" Array position "));
+  debugPrintLn(bufferLocation); // print the buffer location that is used
+  for (int i = 0; i < 4; i++)
+    plBuffer[bufferLocation].id[i] = payloadAddress->id[i];
+  for (int i = 0; i < numberOfSensors; i++)
+    plBuffer[bufferLocation].temp[i] = payloadAddress->temp[i];
+  plBuffer[bufferLocation].humidity = payloadAddress->humidity;
+  plBuffer[bufferLocation].bat = payloadAddress->bat;
+}
+
+void saveDataToEep(Payload_t *payloadAddress){
+    uint8_t eepRomLocation = 1;
+  // get next free buffer location
+  for (int i = 1; i < BUFFERSIZE; i++) {
+    char ad = 0;
+    int b = 1;
+    rtc.eeprom_read((i*25),ad,b);
+    if (ad != 0)
+      eepRomLocation++; //increase buffer location
+    else
+      break; // use current location
+  }
+  
+
+//// Getting data //////////////////////////////////////////////////////////////
+void checkForNetworkData() {
+  network.update();
+  RF24NetworkHeader header;
+  Payload_t payload;
+
+  while (network.available()) { // Any data on the network ready to read
+    network.read(header, &payload, sizeof(payload));
+    debug2Print(F(" Node ID: "));
+    for (byte b : payload.id)
+      debug2PrintHex(b);
+    debug2PrintLn();
+    fillBufferArray(&payload);
+  }
 }
 
 /////////////// LOOP ///////////////////////////////////////////////////////////
@@ -146,191 +314,12 @@ void loop() {
     nextSend = rtc.minute() + interval;
     if (nextSend >= 60)
       nextSend = nextSend - 60;
-    Serial.println(F("timer tripped"));
-    LocalData_t localData;
-    getLocalData(&localData);
-    getScaleData(&localData);
-    sendMqttData(&localData);
-    clearpayloadBuffer();
+    debugPrintLn(F("timer tripped"));
+    getScaleData();
+    sendScaleMqtt();
   }
-  Serial.println(F("sleep"));
-  delay(500); // give serial time to complete before node goes to sleep
+  debugPrintLn(F("sleep"));
+  Serial.write('S');
+  delay(250); // give serial time to complete before node goes to sleep
   network.sleepNode(15, 0); // 15 cycles of 4 seconds
-}
-
-//// Getting data //////////////////////////////////////////////////////////////
-void checkForNetworkData() {
-  network.update();
-  RF24NetworkHeader header;
-  Payload_t payload;
-
-  while (network.available()) { // Any data on the network ready to read
-    network.read(header, &payload, sizeof(payload));
-    Serial.print(F(" Node ID: "));
-    for (byte b : payload.id)
-      Serial.print(b, HEX);
-    Serial.println();
-    fillBufferArray(&payload);
-  }
-}
-
-void fillBufferArray(Payload_t *payloadAddress) {
-  uint8_t bufferLocation = 0;
-  // get next free buffer location
-  for (int i = 0; i < BUFFERSIZE; i++) {
-    if (payloadBuffer[bufferLocation].temp[0] != 0)
-      bufferLocation++;
-  }
-  Serial.print(F(" Array position "));
-  Serial.println(bufferLocation); // print the buffer location that is used
-  for (int i = 0; i < 4; i++)
-    payloadBuffer[bufferLocation].id[i] = payloadAddress->id[i];
-  for (int i = 0; i < numberOfSensors; i++)
-    payloadBuffer[bufferLocation].temp[i] = payloadAddress->temp[i];
-  payloadBuffer[bufferLocation].humidity = payloadAddress->humidity;
-  payloadBuffer[bufferLocation].bat = payloadAddress->bat;
-}
-
-void saveDataToEep(Payload_t *payloadAddress){
-    uint8_t eepRomLocation = 1;
-  // get next free buffer location
-  for (int i = 1; i < BUFFERSIZE; i++) {
-    char ad = 0;
-    rtc.eeprom_read(i*25,ad,1);
-    if (ad != 0)
-      eepRomLocation++; //increase buffer location
-    else
-      break; // use current location
-  }
-  
-  Serial.print(F(" Eeprom position "));
-  Serial.println(eepRomLocation*25); // print the buffer location that is used
-  for (int i = 0; i < 4; i++)
-    rtc.eeprom_write((eepRomLocation*25)+i,payloadAddress->id[i]);
-  for (int i = 0; i < numberOfSensors; i++)
-    rtc.eeprom_write(30+(eepRomLocation*10)+(i*2),payloadAddress->temp[i]);
-  rtc.eeprom_write(42+(eepRomLocation*10), payloadAddress->humidity);
-  rtc.eeprom_write(44+(eepRomLocation*10), payloadAddress->bat);
-}
-
-void getLocalData(LocalData_t *local) {
-  for (uint8_t i = 0; i < 4; i++) // fill coordinatorId
-    local->baseId[i] = coordId[i];
-  local->baseTemp = 123;//myHumidity.readTemperature() * 100;
-  local->baseHum = 123;//myHumidity.readHumidity() * 100;
-  local->baseBat = battery.getVoltage() * 100; // Battery
-  local->baseLux = 1234; //lightMeter.readLightLevel();
-}
-
-void getScaleData(LocalData_t *local) {
-  Wire.requestFrom(1, 24);
-  delay(100);
-  int i = 0;
-  while (Wire.available()&& i < 22) {
-    s1[i] = Wire.read();
-    i++;
-  }
-  Wire.requestFrom(1, 24);
-  delay(100);
-  i = 0;
-  while (Wire.available()&& i < 22) {
-    s2[i] = Wire.read();
-    i++;
-  }
-  s1[23] = 0;
-  s2[23] = 0;
-
-  for (size_t i = 0; i < 23; i++) {
-  Serial.print(s1[i]);
-  }
-  Serial.println();
-  Serial.println(s1);
-  Serial.println(s2);
-}
-
-//////////// MQTT Code /////////////////////////////////////////////////////////
-void registerNodeMqtt() {
-  gprsResetModem();
-  gprsConnectNetwork();
-  if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
-    mqtt.publish("c/r", mqttClient);
-  }
-  gprsEnd();
-}
-
-void sendMqttData(LocalData_t *local) {
-  gprsResetModem();
-  gprsConnectNetwork();
-  for (int b = 0; b < BUFFERSIZE; b++) {
-    if (payloadBuffer[b].temp[0] != 0) {
-      Serial.print(F("Send buffer "));
-      Serial.println(b);
-      if (mqtt.connect(mqttClient, mqttUser, mqttPswd)) {
-        char buf[120] = ""; // als ik deze als globale variabele declareer werkt het ook niet
-        sprintf(buf, "%02X%02X%02X%02X,%d,%d,%d,%d,%d,%d,%u,%u,%u,%u,%d", payloadBuffer[b].id[0], payloadBuffer[b].id[1], payloadBuffer[b].id[2], payloadBuffer[b].id[3], payloadBuffer[b].temp[0], payloadBuffer[b].temp[1], payloadBuffer[b].temp[2], payloadBuffer[b].temp[3], payloadBuffer[b].temp[4], payloadBuffer[b].temp[5],payloadBuffer[b].humidity,payloadBuffer[b].bat,local->baseTemp,local->baseHum,local->baseLux);
-        mqtt.publish("h/d", buf);
-        // als ik deze lijnen uncomment werkt het ook niet
-        //char buf2[80] = "";
-        //sprintf(buf2, "%s,%s,%s", mqttClient,s1,s2);
-        //mqtt.publish("c/s", buf2);
-      }
-      /*
-      data = data + String(local->baseHum)
-      data = data + String(local->baseLux)
-      */
-    }
-  }
-  mqtt.disconnect();
-  gprsEnd();
-}
-
-//////////// init gprs, connect and disconnect from network ////////////////////
-void gprsResetModem() {
-  modem.restart();
-  String modemInfo = modem.getModemInfo();
-  Serial.print(F(" Modem: "));
-  Serial.println(modemInfo);
-}
-
-void gprsConnectNetwork() {
-  Serial.print(F(" Waiting for network..."));
-  if (!modem.waitForNetwork()) {
-    Serial.println(F(" fail"));
-    delay(10000);
-    return;
-  }
-  Serial.println(F(" OK"));
-  Serial.print(F(" Connecting to "));
-  Serial.print(apn);
-  if (!modem.gprsConnect(apn, user, pass)) {
-    Serial.println(F(" fail"));
-    delay(10000);
-    return;
-  }
-  Serial.println(F(" OK"));
-}
-
-void gprsEnd() {
-  modem.gprsDisconnect();
-}
-
-//////////// init nrf radio ////////////////////////////////////////////////////
-void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
-  Serial.print(F("Channel: "));
-  Serial.print(channel);
-  Serial.print(F(", Node: "));
-  Serial.println(nodeAddress);
-
-  SPI.begin();
-  radio.begin();
-  // radio.setPALevel(HIGH);
-  network.begin(channel, nodeAddress);
-  network.setup_watchdog(9);
-}
-
-//////////// Misc Functions ////////////////////////////////////////////////////
-void clearpayloadBuffer() {
-  for (int i = 0; i < BUFFERSIZE; i++) {
-    payloadBuffer[i].temp[0] = 0;
-  }
 }
