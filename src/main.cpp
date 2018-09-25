d1e0c56ed129f422e2ba778940e5234783023b14
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-09-25 06:20:07 +0200

    commented all RF24 code

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 #define softSerialAtTx 5
 #define resetPin A2 // resetting esp/gprs module
 
-#define MISO 11
-#define MOSI 12
-#define CLK 13
+#define MISO 11 //
+#define MOSI 12 //
+#define CLK 13 //
 
 #define seedRef A3 // seed for random function
 
@@ -36,12 +36,12 @@ PubSubClient mqtt(client);
 ///////////////////////////////////// MQTT specific ////////////////////////////
 char mqttCl[12] = "";
 ///////////////////////////////////// RADIO ////////////////////////////////////
-#include <RF24.h>
-#include <RF24Network.h>
-#include <SPI.h>
-RF24 radio(radioPin1, radioPin2); // start RF24 communication layer
-RF24Network network(radio);       // start RF24 network layer
-const uint16_t thisNode = 00;     // Coordinator address
+//#include <RF24.h>
+//#include <RF24Network.h>
+//#include <SPI.h>
+//RF24 radio(radioPin1, radioPin2); // start RF24 communication layer
+//RF24Network network(radio);       // start RF24 network layer
+//const uint16_t thisNode = 00;     // Coordinator address
 ////////////////////////////////////////////////////////////////////////////////
 #include <Wire.h>
 ///////////////////////////////////// HUMIDITY /////////////////////////////////
@@ -70,21 +70,21 @@ MesureVoltageInternal battery(iREF);
 
 ///////////////////////////////////// STRUCTS & STRUCT ARAYS ///////////////////
 // Structure of our payload coming from router and end devices
-struct Payload_t {
-  uint8_t id[4];
-  int16_t temp[6];
-  uint16_t bat;
-  uint16_t weight;
-  uint16_t humidity;
-  uint8_t alarm;
-};
+//struct Payload_t {
+//  uint8_t id[4];
+//  int16_t temp[6];
+//  uint16_t bat;
+//  uint16_t weight;
+//  uint16_t humidity;
+//  uint8_t alarm;
+//};
 
-struct plBuffer_t {
-  uint8_t id[4];
-  int16_t temp[6];
-  uint16_t bat;
-  uint16_t humidity;
-};
+//struct plBuffer_t {
+//  uint8_t id[4];
+//  int16_t temp[6];
+//  uint16_t bat;
+//  uint16_t humidity;
+//};
 
 struct LocalData_t {
   uint8_t baseId[4];
@@ -96,14 +96,14 @@ struct LocalData_t {
 };
 
 // Globalstruct array to collect data before Sending
-#define BUFFERSIZE 6
-plBuffer_t plBuffer[BUFFERSIZE];
+//#define BUFFERSIZE 6
+//plBuffer_t plBuffer[BUFFERSIZE];
 ////////////////////////////////////////////////////////////////////////////////
-#define numberOfSensors 6
+//#define numberOfSensors 6
 
 ////////////////////////// FUNCTION DECLARATIONS ///////////////////////////////
 // getting data
-void fillBufferArray(Payload_t *payloadAddress);
+//void fillBufferArray(Payload_t *payloadAddress);
 void getLocalData(LocalData_t *local);
 void getScaleData(LocalData_t *local);
 // gprs network functions
@@ -115,10 +115,10 @@ void registerNodeMqtt();
 // MQTT specific
 void sendMqttData(LocalData_t *local);
 // radio functions
-void initRFRadio(uint8_t channel, uint16_t nodeAddress);
-void checkForNetworkData();
+//void initRFRadio(uint8_t channel, uint16_t nodeAddress);
+//void checkForNetworkData();
 // misc Functions
-void clearplBuffer();
+//void clearplBuffer();
 
 /////////////// SETUP //////////////////////////////////////////////////////////
 void setup() {
@@ -139,21 +139,21 @@ void setup() {
   battery.setRefInternal();
   myHumidity.begin();
   lightMeter.begin(BH1750::ONE_TIME_HIGH_RES_MODE);
-  initRFRadio(90, thisNode); // start nRF24l radio
+  //initRFRadio(90, thisNode); // start nRF24l radio
 
   mqtt.setServer(broker, mqttPort);
 
   registerNodeMqtt();
 
-  clearplBuffer();
+  //clearplBuffer();
   rtc.refresh();
   nextSend = rtc.minute();
-  //Serial.println(F("init complete"));
+  Serial.println(F("init complete"));
 }
 
 /////////////// LOOP ///////////////////////////////////////////////////////////
 void loop() {
-  checkForNetworkData();
+  //checkForNetworkData();
   rtc.refresh();
   if (nextSend <= rtc.minute()) {
     nextSend = rtc.minute() + interval;
@@ -164,14 +164,15 @@ void loop() {
     getLocalData(&localData);
     getScaleData(&localData);
     sendMqttData(&localData);
-    clearplBuffer();
+    //clearplBuffer();
   }
   Serial.println(F("sleep"));
   delay(500); // give serial time to complete before node goes to sleep
-  network.sleepNode(15, 0); // 15 cycles of 4 seconds
+  //network.sleepNode(15, 0); // 15 cycles of 4 seconds
 }
 
 //// Getting data //////////////////////////////////////////////////////////////
+/*
 void checkForNetworkData() {
   network.update();
   RF24NetworkHeader header;
@@ -186,7 +187,9 @@ void checkForNetworkData() {
     fillBufferArray(&payload);
   }
 }
+*/
 
+/*
 void fillBufferArray(Payload_t *payloadAddress) {
   uint8_t bufferLocation = 0;
   // get next free buffer location
@@ -203,6 +206,7 @@ void fillBufferArray(Payload_t *payloadAddress) {
   plBuffer[bufferLocation].humidity = payloadAddress->humidity;
   plBuffer[bufferLocation].bat = payloadAddress->bat;
 }
+*/
 
 void getLocalData(LocalData_t *local) {
   for (uint8_t i = 0; i < 4; i++) // fill coordinatorId
@@ -243,7 +247,7 @@ void registerNodeMqtt() {
 void sendMqttData(LocalData_t *local) {
   gprsResetModem();
   gprsConnectNetwork();
-  for (int b = 0; b < BUFFERSIZE; b++) {
+  /*for (int b = 0; b < BUFFERSIZE; b++) {
     if (plBuffer[b].temp[0] != 0) {
       Serial.print(F("Send buffer "));
       Serial.println(b);
@@ -252,12 +256,8 @@ void sendMqttData(LocalData_t *local) {
         sprintf(buf, "%02X%02X%02X%02X,%d,%d,%d,%d,%d,%d,%u,%u,%u,%u,%d", plBuffer[b].id[0], plBuffer[b].id[1], plBuffer[b].id[2], plBuffer[b].id[3], plBuffer[b].temp[0], plBuffer[b].temp[1], plBuffer[b].temp[2], plBuffer[b].temp[3], plBuffer[b].temp[4], plBuffer[b].temp[5],plBuffer[b].humidity,plBuffer[b].bat,local->baseTemp,local->baseHum,local->baseLux);
         mqtt.publish("h/d", buf);
       }
-      /*
-      data = data + String(local->baseHum)
-      data = data + String(local->baseLux)
-      */
     }
-  }
+  }*/
   mqtt.disconnect();
   gprsEnd();
 }
@@ -293,6 +293,7 @@ void gprsEnd() {
 }
 
 //////////// init nrf radio ////////////////////////////////////////////////////
+/*
 void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
   Serial.print(F("Channel: "));
   Serial.print(channel);
@@ -305,10 +306,13 @@ void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
   network.begin(channel, nodeAddress);
   network.setup_watchdog(9);
 }
+*/
 
 //////////// Misc Functions ////////////////////////////////////////////////////
+/*
 void clearplBuffer() {
   for (int i = 0; i < BUFFERSIZE; i++) {
     plBuffer[i].temp[0] = 0;
   }
 }
+*/
